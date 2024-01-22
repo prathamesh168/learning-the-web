d443ca56d7802dc1ea000043debb27fb83512145
Prathamesh Mulay <76168141+prathamesh168@users.noreply.github.com>
2024-01-22 14:48:52 +0000

    intial routes

--- proj-2/Party-chiefs.js
+++ proj-2/Party-chiefs.js
@@ -1,10 +1,14 @@
-
+const mongoose = require("mongoose")
 const cors = require("cors");
 const bodyparser = require('body-parser')
+const jwt = require("jsonwebtoken")
 const exp = require("express")
-const fs = require("fs")
+const fs = require("fs");
 const app = exp();
 const Port = 3009
+const mongoURI = 'mongodb+srv://<username>:<password>@cheifandcustomer.u6pxjjm.mongodb.net/';
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 app.use(cors());
 app.use(bodyparser.json())
 
@@ -12,12 +16,29 @@ app.get("/" , (req,res)=> {
    console.log(" this is homepage ")
    res.send("this is homepage")
 })
+const secretKey = "Customer is god"
+const generatetoken = (user)=>{
+    const payload = {
+        username : user.username,
+        email : user.email,
+        password : user.password};
+    const ans = jwt.sign(payload, secretKey,{expiresIn:"1h"})
+    return ans
+}
+
+const Authenticateme = (req,res,next) =>{
+    const username = req.headers.username;
+    const email = req.headers.email;
+    const password = req.headers.password;
+
+    next();
+}
 
 
 
-app.post("/admin/signup" , (req,res)=> {
-    console.log(" this is homepage ")
-    res.send("this is homepage")
+app.post("/admin/signup" ,Authenticateme, (req,res)=> {
+    const {username , email , password} = req.body
+    
  })
 
 app.post("/admin/signin" , (req,res)=> {
